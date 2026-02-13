d9346331b4799aaa46bc04b5d1573046cfe86698
Pat Bills <billspat@msu.edu>
2026-02-13 15:58:41 -0500

    organize config file with I/O locations

--- python/fod_config.py
+++ python/fod_config.py
@@ -1,44 +1,63 @@
-INPUT_FILE = "/home/web-ewx/webdir/legacy/mioffset/footprint_input.txt" 
 
-RM_OUTPUT_FILES = "/home/web-ewx/webdir/legacy/mioffset/tmp" 
 
-NARR_DATA_FOLDER = "/data/mtkiefer/ODOR/NARR_DATA/" 
-
+#######
+# HDF5 input data files 
 NARR_INPUT = "/data/mtkiefer/ODOR/NARR_DATA/narr_latlon.h5" 
 
 NARR_INPUT_LOC = "/data/mtkiefer/ODOR/NARR_DATA/narr_PSD_" 
 
-OUT_IMG_3_1_FY = "/home/web-ewx/webdir/legacy/mioffset/tmp/image_footprint_3inone_FY.png" 
 
-OUT_IMG_3_1_WS = "/home/web-ewx/webdir/legacy/mioffset/tmp/image_footprint_3inone_WS.png" 
-
-OUT_IMG_FY = "/home/web-ewx/webdir/legacy/mioffset/tmp/image_footprint_FY.png" 
-
-OUT_IMG_WS = "/home/web-ewx/webdir/legacy/mioffset/tmp/image_footprint_WS.png" 
-
-SETBACK_FY = '/home/web-ewx/webdir/legacy/mioffset/tmp/table_setbackdistance_FY.txt' 
-
-SETBACK_WS = '/home/web-ewx/webdir/legacy/mioffset/tmp/table_setbackdistance_WS.txt' 
-
-PLACE_MARK = 'http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png' 
-
-SAVE_FOOTPRINT_FY = "/home/web-ewx/webdir/legacy/mioffset/tmp/kml_footprint_FY.kml" 
-
-SAVE_FOOTPRINT_WS = "/home/web-ewx/webdir/legacy/mioffset/tmp/kml_footprint_WS.kml" 
-
-SHAPE_SOURCE_FY = '/home/web-ewx/webdir/legacy/mioffset/tmp/shp_source_FY' 
-
-SHAPE_SOURCE_WS = '/home/web-ewx/webdir/legacy/mioffset/tmp/shp_source_WS' 
-
-SHAPE_FOOTPRINT_FY = '/home/web-ewx/webdir/legacy/mioffset/tmp/shp_footprint_FY' 
-
-SHAPE_FOOTPRINT_WS = '/home/web-ewx/webdir/legacy/mioffset/tmp/shp_footprint_WS' 
-
-OUTPUT_LOCATION = '../output
-
+####
+# base location to store output files
 OUTPUT_OFFSET_DIR = '../tmp/'   # This is needed for creating the zip file; It is relative to the executable 
 
-  
+#####
+# files and folders used to save output from script 
+OUT_IMG_3_1_FY = OUTPUT_OFFSET_DIR + "image_footprint_3inone_FY.png" 
+
+OUT_IMG_3_1_WS = OUTPUT_OFFSET_DIR + "image_footprint_3inone_WS.png" 
+
+OUT_IMG_FY = OUTPUT_OFFSET_DIR + "image_footprint_FY.png" 
+
+OUT_IMG_WS = OUTPUT_OFFSET_DIR + "image_footprint_WS.png" 
+
+SETBACK_FY = OUTPUT_OFFSET_DIR + 'table_setbackdistance_FY.txt' 
+
+SETBACK_WS = OUTPUT_OFFSET_DIR + 'table_setbackdistance_WS.txt' 
+
+SAVE_FOOTPRINT_FY = OUTPUT_OFFSET_DIR + "kml_footprint_FY.kml" 
+
+SAVE_FOOTPRINT_WS = OUTPUT_OFFSET_DIR + "kml_footprint_WS.kml" 
+
+SHAPE_SOURCE_FY = OUTPUT_OFFSET_DIR + 'shp_source_FY' 
+
+SHAPE_SOURCE_WS = OUTPUT_OFFSET_DIR + 'shp_source_WS' 
+
+SHAPE_FOOTPRINT_FY = OUTPUT_OFFSET_DIR + 'shp_footprint_FY' 
+
+SHAPE_FOOTPRINT_WS = OUTPUT_OFFSET_DIR + 'shp_footprint_WS' 
+
+
+# used to save shapefile only, which are then zipped and 
+# store in maybe another location
+OUTPUT_LOCATION = '../output'
+
+#### 
+# url for map making
+PLACE_MARK = 'http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png' 
+
+
+########
+# unused
+# ?
+INPUT_FILE = "/home/web-ewx/webdir/legacy/mioffset/footprint_input.txt" 
+# the NARR files are explicitly named in the fod3.py code, 
+NARR_DATA_FOLDER = "/data/mtkiefer/ODOR/NARR_DATA/" 
+# this _was_ used to identify the folder where old files were to be deleted
+# but the code to delete the existing files is commented out
+RM_OUTPUT_FILES = "/home/web-ewx/webdir/legacy/mioffset/tmp" 
+
+########
 # program paramaters defaults/testing?  unknown
 # Eventually, the values below will all be passed by the web page, thus we will be able to comment them out 
 
