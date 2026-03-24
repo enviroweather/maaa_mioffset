930b22064ebafdbdd1070f4b168ca0d330b6fbcb
Pat Bills <billspat@msu.edu>
2026-03-24 12:34:34 -0400

    shell to test  py script requireing cli args

---
+++ python/fod_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "using python $(which python)"
+latval=43.14319
+lonval=-84.23689
+odor_index=10
+# python version of getting time stamp
+# from datetime import datetime
+# time_stamp = datetime.now().strftime("%H:%M:%S.%f")
+# linux 
+if [[ "$(uname)" == "Darwin" ]]; then
+    time_stamp=$(date '+%H:%M:%S.%N')
+else
+    time_stamp=$(date '+%H:%M:%S.%f')
+fi
+# time_stamp = $(date '+%H:%M:%S.%f')
+# macOS/BSD: date '+%H:%M:%S.%N'
+echo "latval : $latval"
+echo "lonval : $lonval"
+echo "odor_index : $odor_index"
+echo "time_stamp : $time_stamp"
+python fod3.py  $latval  $lonval  $odor_index "$time_stamp"
+
