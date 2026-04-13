18b147f7c225e17744ec37752be00b6b10324ad6
Pat Bills <billspat@msu.edu>
2026-04-13 09:17:40 -0400

    sc-1127 WIP transform refactor for 1 x coord at at time

--- python/narr_data_transform.py
+++ python/narr_data_transform.py
@@ -3,20 +3,12 @@
 import os, sys
 import json
 
-# requirements python_dotenv, h5py, boto3, numpy
-print('importing libs')
+# requirements python_dotenv, h5py, boto3, optional numpy
 from dotenv import load_dotenv
 import h5py
 # this is only needed when reading data in
 #import numpy as np
-
-
-print('import aws module')
-
 from aws import get_s3_client, check_bucket, get_aws_config
-
-
-print("importing fod3 file which takes forever")
 from fod3 import path_to_narrfile # read_narr_lat_lon,validate_latlon,read_one_year,read_narr_timeseries, 
 
 def read_one_year_grid(yr:str,narr_input_dir:str):
@@ -36,64 +28,49 @@ def read_one_year_grid(yr:str,narr_input_dir:str):
     return(h5f)
 
 
-print("AWS setup")
-load_dotenv()
-s3_client = get_s3_client()  # use default dot-env
-narr_bucket = os.getenv('BUCKET_NAME')
-if not check_bucket(s3_client, narr_bucket):
-    sys.exit(1)
- 
-# const
-years = list(range(1979, 2009))
-
-## set up narr files
-narr_input_dir = h5folder = os.getenv('NARR_INPUT_DIR')
-if not os.path.exists(path_to_narrfile(2001, narr_input_dir)):
-    print("can't access NARR files")
-    sys.exit(1)
+def build_grid_coordinates(grided_data, grid_x=None, grid_y=None):
+    """create a list of 2D coordinate tuples (x,y) given a grid file
     
 
-##########
-print("reading narr data")
-narr_data = {}
-for yr in years:
-    print(yr)
-    narr_data[yr] = read_one_year_grid(yr, narr_input_dir)
- 
-########## 
-print("build coordinates")
-# one_year = read_one_year_grid(1979, narr_input_dir=narr_input_dir)
-
-one_year = narr_data[years[0]]['PC']
-
-grid_size_x = one_year.shape[0]
-grid_size_y = one_year.shape[1]
-       
-xdx = list(range(grid_size_x))
-ydx = list(range(grid_size_y))
-
-# loop just cause it's very clear 
-coords = []
-for a in xdx:
-    for b in  ydx:
-        coords.append( [ a, b ] )
+    Args:
+        grided_data (_type_): grid hd5 file with x and y as first two elements
+        
+        grid_x (int or list, optional): either a single int or a list of ints that
+        will be used for the x-coordinates for creating a subset of the list. Defaults to None which means use whole grid
+        grid_y (int or list, optional): either a single int or a list of ints that
+        will be used for the y-coordinates for creating a subset of the list. Defaults to None which means use whole grid   
+    """
     
-grid_checkpoint = os.getenv('GRID_CHECKPOINT')
-if grid_checkpoint:
-   try:
-       grid_checkpoint = json.loads(grid_checkpoint)
-       if  type(grid_checkpoint) == type(list()) and len(grid_checkpoint) == 2:	
-           coords = list(filter(lambda coord: coord > grid_checkpoint,  coords))
-   except Exception as e:
-      print(f"couldn't use checkpoint {grid_checkpoint} so stopping.  clear variable or fix to look like [1,2]")
+    # is there a better way than to filter the list here when we make the 
+    # coordinate list? this wil work for this one-off
+    
+    # previously got the grid from reading a single year and filtering
+    # one_year = read_one_year_grid(1979, narr_input_dir=narr_input_dir)
+    # then it was 
+    # one_year = narr_data[years[0]]['PC']
+    grid_size_x = grided_data.shape[0]
+    grid_size_y = grided_data.shape[1]
 
 
-######
-print(" main data transform loop")
-## we  3 datasets from the F5 files, in single dict narr_data[yr][dataset][x][y](ts)
-datasets = ['PC', 'WS', 'WD']
-
-# check these are all in the h5 file! 
+    if grid_x:
+        xdx = list(grid_x)
+    else:
+        xdx = list(range(grid_size_x))
+        
+    if grid_y:
+        ydx = list(grid_y)
+    else:
+        ydx = list(range(grid_size_y))
+        
+    # loop to build list of tuples from cartesian product 
+    coords = []
+    for a in xdx:
+        for b in  ydx:
+            coords.append( ( a, b ) )
+    
+    return(coords)
+            
+            
 
 def narr_filename(dataset:str, x:int,y:int ):
     """
@@ -103,30 +80,103 @@ def narr_filename(dataset:str, x:int,y:int ):
     return(one_coord_filename)
 
 
-for coord in coords:
-    print(coord)
-    x,y = coord
+def transform_by_coordinate(grid_x, grid_y=None, narr_bucket=None, config=None):
+
+    load_dotenv()
     
-    # extract one coordinate's timeseries' for all years per dataset
-    # into new dict structure
-    one_coord = {'PC': {}, 'WS': {}, 'WD': {}}
+    print("AWS setup")
+    
+    s3_client = get_s3_client()  # use default dot-env
+    
+    # check that the bucket is in there
+    if not narr_bucket:
+        narr_bucket = os.getenv('BUCKET_NAME')
+    if not check_bucket(s3_client, narr_bucket):
+        sys.exit(1)
+ 
+    # constants
+    years = list(range(1979, 2009))
+    datasets = ['PC', 'WS', 'WD']
+    
+
+    ## set up narr files
+    narr_input_dir = h5folder = os.getenv('NARR_INPUT_DIR')
+    if not os.path.exists(path_to_narrfile(2001, narr_input_dir)):
+        print("can't access NARR files")
+        sys.exit(1)
+    
+
+    ##########
+    print("reading narr data")
+    narr_data = {}
     for yr in years:
-        print(f"\t {yr}")
+        print(yr)
+        narr_data[yr] = read_one_year_grid(yr, narr_input_dir)
+ 
+    one_year = narr_data[years[0]]['PC']
+    coords = build_grid_coordinates(one_year)
+
+    ######
+    print(" main data transform loop")
+    ## we  3 datasets from the F5 files, in single dict narr_data[yr][dataset][x][y](ts)
+
+    # check these are all in the h5 file! 
+
+    for coord in coords:
+        print(coord)
+        x,y = coord
+        
+        # extract one coordinate's timeseries' for all years per dataset
+        # into new dict structure
+        one_coord = {'PC': {}, 'WS': {}, 'WD': {}}
+        for yr in years:
+            print(f"\t {yr}")
+            for dataset in datasets :
+                # timeseries data read in as a NP value, convert these to float new storage
+                one_coord[dataset][yr] = list( map(float, narr_data[yr][dataset][x][y]) )
+
+        # save 3 files in s3 for each dataset for this coordinate
         for dataset in datasets :
-            # timeseries data read in as a NP value, convert these to float new storage
-            one_coord[dataset][yr] = list( map(float, narr_data[yr][dataset][x][y]) )
+            try:
+                resp = s3_client.put_object(
+                    Body = json.dumps(one_coord[dataset]),
+                    Bucket = narr_bucket,
+                    Key = narr_filename(dataset, x,y)
+                )
+                
+                if resp['ResponseMetadata']['HTTPStatusCode'] != 200:
+                    print(f"error writing to S3 for coordinates ({x}, {y}) and dataset {dataset}")
+                    return False
+                else:
+                    print(f"({x}, {y}) {dataset}")
+            except Exception as e:
+                print(f"error S3 {e} for ({x}, {y}) dataset {dataset}")
+                return False
+            
+        return True
 
-    # save 3 files in s3 for each dataset for this coordinate
-    for dataset in datasets :
-        print(s3_client.put_object(
-            Body = json.dumps(one_coord[dataset]),
-            Bucket = narr_bucket,
-            Key = narr_filename(dataset, x,y)
-        ))
 
+if __name__ == "__main__":
+    
+    # load config from environment, .env file
+    result = load_dotenv()
+    if not result:
+        print("could not load .env file")
+        sys.exit(1)
+        
+    if len(sys.argv) > 1:
+        x_coordinate = float(sys.argv[1])
+    else:
+        x_coordinate = None
+    
+    result = transform_by_coordinate(grid_x = x_coordinate, narr_bucket=os.getenv('BUCKET_NAME'))
+    if not result:
+        print(f"ERROR x = {x_coordinate}")
+        sys.exit(1)
+    
+    
 
-
-########## DATA READ FUNCTIONS FOR NEW STRUCTUR
+########## DATA READ FUNCTIONS FOR NEW STRUCTURE
 # move these to FOD program or narr python file
 def read_dataset_from_file(grid_x:int, grid_y:int, dataset:str,narr_input_dir:str):
     """read a dataset for all years, one coordinate from s3
@@ -168,6 +218,7 @@ def read_dataset_from_s3(grid_x:int, grid_y:int, dataset:str, bucket:str, s3_cli
     return ts_by_year
 
 
+# requires numpy
 def prep_dataset_for_fod(ts_by_year: dict[int, float]):
     """convert dictionary of timeserize by year into single
     np array with them all smashed together
@@ -179,6 +230,9 @@ def prep_dataset_for_fod(ts_by_year: dict[int, float]):
         np.array: time series of floats as expected by FOD 
     """
     
+    if 'numpy' not in sys.modules:
+        import numpy as np
+        
     ts_by_year_nparray = list(map(np.array, list(ts_by_year.values())))
     ts_by_year_merged = np.array(np.concatenate(ts_by_year_nparray))
     return ts_by_year_merged
