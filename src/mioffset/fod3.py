0be273450d4c1af5f5db920557231527da95eff6
Pat Bills <billspat@msu.edu>
2026-06-01 10:14:04 -0400

    only import what we need from numpy sc-2030

--- src/mioffset/fod3.py
+++ src/mioffset/fod3.py
@@ -11,6 +11,10 @@
 #   Modified by 
 #
 #	alterations for using in PHP application by Tracy Aichle
+#   June 2026 (Pat Bills, MSU ICER)
+#     +/- organized into distinct modules to minimize inputs needs for cloud functions
+#     + distinction between mioffset for web (dictionary and GeoJSON only) and for "report"
+#       which has plots and GIS Files if and when needed to make web site very responsive
 #   May 2026 (Pat Bills, MSU ICER)
 #     + added methods create other map and image formats for API outputs
 #     + split functions to 1) create object 2) save to disk OR 3) convert for api
@@ -55,7 +59,9 @@ load_dotenv()
 
 # for model
 from math import pow
-import numpy as np
+from numpy import ndarray, array, copy, round, arange, empty, random, zeros, histogram, median, sum, where
+from numpy import min as np_min  # to not mask python min
+from numpy import max as np_max  
 from geopy.distance import geodesic
 
 # for mapping
@@ -79,19 +85,19 @@ logging.log(logging.INFO, "MIOFFSET DEVELOPMENT VERSION - NOT FOR PRODUCTION USE
 
 
 
-def setback_text_table(D: np.ndarray)->str:
+def setback_text_table(D: ndarray)->str:
     """ create Special special version of D array, with three "N" rows at top of table and other 
     two "N" rows at bottom of table.  This is done in order to match how it 
     is presented in existing MI Odor Print excel spreadsheet.
     
     Args:
-        D (np.ndarray): offset values, must have shape 80,3
+        D (ndarray): offset values, must have shape 80,3
     
     Returns:
         str: lines of table suitable for printing in fixed width font
     """
     
-    wlab=np.array(
+    wlab=array(
        ['N','-','-','-','-','NNE','-','-','-','-','NE','-','-','-','-', \
 	    'ENE','-','-','-','-','E','-','-','-','-','ESE','-','-','-','-', \
 	    'SE','-','-','-','-','SSE','-','-','-','-','S','-','-','-','-', \
@@ -103,14 +109,14 @@ def setback_text_table(D: np.ndarray)->str:
     # Special special version of D array, with three "N" rows at top of table and other 
     # two "N" rows at bottom of table.  This is done in order to match how it 
     # is presented in existing MI Odor Print excel spreadsheet.
-    Dtbl=np.copy(D)
+    Dtbl=copy(D)
     Dtbl[1:79,:]=D[0:78,:]
     Dtbl[0]=D[79,:]
     
     # round to 2 places
-    d5 = np.round(Dtbl[:,0],2)   # 5%
-    d3 = np.round(Dtbl[:,1],2)   # 3%
-    d15 = np.round(Dtbl[:,2],2)  # 1.5%
+    d5 = round(Dtbl[:,0],2)   # 5%
+    d3 = round(Dtbl[:,1],2)   # 3%
+    d15 = round(Dtbl[:,2],2)  # 1.5%
     
     header_lines = [
         f"{'Toward Distance_in_Miles':>6}",
@@ -127,7 +133,7 @@ def setback_text_table(D: np.ndarray)->str:
     return(table_text)
     
 
-def fod_plot_to_ll(D, lat:float, lon:float)->np.ndarray:
+def fod_plot_to_ll(D, lat:float, lon:float)->ndarray:
     """convert setback distance output from FOD model into a the 
     lat, lon coordinates of set-back radius from the 
     center point for placing on a map 
@@ -138,9 +144,9 @@ def fod_plot_to_ll(D, lat:float, lon:float)->np.ndarray:
         lon (float): longitude of center point
     """
     
-    dbin = np.arange(4.5, 364.5, 4.5) #redefined with 4.5 degree bins.        
+    dbin = arange(4.5, 364.5, 4.5) #redefined with 4.5 degree bins.        
     
-    LL:np.ndarray = np.empty((81,3,2), dtype=float, order='F')        
+    LL:ndarray = empty((81,3,2), dtype=float, order='F')        
     
     for d in range(0,dbin.size):
         for p in range(0,3):
@@ -158,34 +164,34 @@ def fod_plot_to_ll(D, lat:float, lon:float)->np.ndarray:
 
 ###### MAIN MODEL 
         
-def fod_model(pc: np.ndarray, wind_speed:np.ndarray, wind_direction:np.ndarray, odor_index:int):
+def fod_model(pc: ndarray, wind_speed:ndarray, wind_direction:ndarray, odor_index:int):
     """calculates an aray of setback distances in miles given wind
     characteristics for a coordinate in the state of Michigan 
 
     Args:
-        pc (np.ndarray): time series of ?
-        wind_speed (np.ndarray): time series of wind speeds
-        wind_direction (np.ndarray): time series of wind directions
+        pc (ndarray): time series of ?
+        wind_speed (ndarray): time series of wind speeds
+        wind_direction (ndarray): time series of wind directions
         odor_index (int): odor index calculated from building size and type
     Returns:
-        np.array 3 sets of 80 values (shape = (80,3)), 5% 3%, 1.5% setback distance
+        array 3 sets of 80 values (shape = (80,3)), 5% 3%, 1.5% setback distance
     """
 
     # renamed for comparability with original code
     E = odor_index
     #-----------------------Wind direction processing------------------------
     
-    indx=np.random.RandomState(seed=8675309).permutation(wind_direction.size)
+    indx=random.RandomState(seed=8675309).permutation(wind_direction.size)
     wd4=[90,180,270,360]
-    i4=np.zeros((wind_direction.size,4), dtype=int, order='F')
-    h,x = np.histogram(wind_direction,bins=np.arange(0,361,1))
+    i4=zeros((wind_direction.size,4), dtype=int, order='F')
+    h,x = histogram(wind_direction,bins=arange(0,361,1))
     for m in range(0,4):
         if(m<3):
-            a1=np.median(h[wd4[m]-1-6:wd4[m]-1-2])
-            a2=np.median(h[wd4[m]-1+2:wd4[m]-1+6])
+            a1=median(h[wd4[m]-1-6:wd4[m]-1-2])
+            a2=median(h[wd4[m]-1+2:wd4[m]-1+6])
         else:
-            a1=np.median(h[wd4[m]-1-6:wd4[m]-1-2])
-            a2=np.median(h[1:5])
+            a1=median(h[wd4[m]-1-6:wd4[m]-1-2])
+            a2=median(h[1:5])
         cap=round((a1+a2)/2)
         I=wind_direction==wd4[m];c=1;i4[:,m]=I.astype(int)
         for t in range(0,i4[:,0].size):
@@ -193,15 +199,15 @@ def fod_model(pc: np.ndarray, wind_speed:np.ndarray, wind_direction:np.ndarray, 
             if((I[tr].astype(int)==1) & (c<=cap)):
                 i4[tr,m]=0
                 c=c+1	
-    Isum=np.sum(i4,1)
+    Isum=sum(i4,1)
     I1=Isum>0
     I2=I1.astype(int)
-    wind_directionds=np.copy(wind_direction)
+    wind_directionds=copy(wind_direction)
     wind_directionds[I2==1]=-999
 
     #--------Footprint preliminary step 1: compute "windstar chart"----------
-    dbin=np.arange(11.25,360,22.5)
-    wc = np.zeros((16,6), dtype=float, order='F')
+    dbin=arange(11.25,360,22.5)
+    wc = zeros((16,6), dtype=float, order='F')
     for d in range(0,dbin.size):
         if (d == 0):									
             pcs = pc[(wind_directionds >= dbin[15]) | ((wind_directionds < dbin[0]) & (wind_directionds >= 0))]
@@ -218,80 +224,80 @@ def fod_model(pc: np.ndarray, wind_speed:np.ndarray, wind_direction:np.ndarray, 
 
 
     #------Footprint preliminary step 2: identify 1.5%,3%,5% classess--------
-    f = np.zeros((5*dbin.size,3), dtype=int, order='F')
+    f = zeros((5*dbin.size,3), dtype=int, order='F')
     for d in range (0,dbin.size):
-        tem=np.round(wc[d,:],2)
+        tem=round(wc[d,:],2)
         if(d==0):
-            f[37:42,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[37:42,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[37:42,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[37:42,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[37:42,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[37:42,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==1):
-            f[42:47,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[42:47,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[42:47,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[42:47,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[42:47,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[42:47,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==2):
-            f[47:52,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[47:52,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[47:52,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[47:52,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[47:52,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[47:52,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==3):
-            f[52:57,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[52:57,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[52:57,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[52:57,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[52:57,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[52:57,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==4):
-            f[57:62,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[57:62,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[57:62,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[57:62,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[57:62,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[57:62,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==5):
-            f[62:67,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[62:67,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[62:67,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
-            f[67:72,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
+            f[62:67,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[62:67,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[62:67,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
+            f[67:72,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
         elif(d==6):
-            f[67:72,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[67:72,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[67:72,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[67:72,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==7):
-            f[72:77,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[72:77,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[72:77,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[72:77,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[72:77,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[72:77,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==8):
-            f[77:80,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[77:80,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[77:80,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
-            f[0:2,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[0:2,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[0:2,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[77:80,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[77:80,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[77:80,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
+            f[0:2,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[0:2,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[0:2,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==9):
-            f[2:7,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[2:7,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[2:7,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[2:7,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[2:7,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[2:7,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==10):
-            f[7:12,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[7:12,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[7:12,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[7:12,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[7:12,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[7:12,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==11):
-            f[12:17,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[12:17,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[12:17,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[12:17,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[12:17,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[12:17,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==12):
-            f[17:22,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[17:22,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[17:22,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[17:22,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[17:22,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[17:22,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==13):
-            f[22:27,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[22:27,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[22:27,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[22:27,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[22:27,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[22:27,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==14):
-            f[27:32,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[27:32,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[27:32,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[27:32,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[27:32,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[27:32,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
         elif(d==15):
-            f[32:37,2]=np.min(np.where(tem==max(tem[tem<=1.5])))+1
-            f[32:37,1]=np.min(np.where(tem==max(tem[tem<=3])))+1
-            f[32:37,0]=np.min(np.where(tem==max(tem[tem<=5])))+1
+            f[32:37,2]=np_min(where(tem==np_max(tem[tem<=1.5])))+1
+            f[32:37,1]=np_min(where(tem==np_max(tem[tem<=3])))+1
+            f[32:37,0]=np_min(where(tem==np_max(tem[tem<=5])))+1
 
 
     #-------Footprint preliminary step 3: compute setback distance (D)-------
-    D = np.zeros((5*dbin.size,3), dtype=float, order='F')
+    D = zeros((5*dbin.size,3), dtype=float, order='F')
     for d in range (0,5*dbin.size):
         for p in range (0,3):
             if (f[d,p] == 1):
@@ -309,12 +315,12 @@ def fod_model(pc: np.ndarray, wind_speed:np.ndarray, wind_direction:np.ndarray, 
                 
     return(D) 
 
-def fod2dict(D:np.ndarray)->dict[str, list[float]]:
+def fod2dict(D:ndarray)->dict[str, list[float]]:
     """convert output of FOD model from Numpy array to python dict, one
     for each column.  This is to help convert to JSON to return via an API
 
     Args:
-        D (np.ndarray): output from fod_model, 80 rows, 2 columns
+        D (ndarray): output from fod_model, 80 rows, 2 columns
 
     Returns:
         dict[str, list[float]]: same date, but one key for each column, keys named by what they are
